ed68a9950b395b59d7db055c59897ddbf03736f7
Ewen Cheslack-Postava <me@ewencp.org>
2014-12-22 15:08:02 -0800

    Archive packaging to tar.gz and zip.
    We don't really need this separately for rest-utils, but this also provides
    baseline packaging which can be used for both rpm and deb.

---
+++ create_archive.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Creates an archive suitable for distribution (standard layout for binaries,
+# libraries, etc.).
+
+set -e
+
+if [ -z ${PACKAGE_TITLE} -o -z ${VERSION} -o -z ${DESTDIR} ]; then
+    echo "PACKAGE_TITLE, VERSION, and DESTDIR environment variables must be set."
+    exit 1
+fi
+
+BINPATH=${PREFIX}/bin
+LIBPATH=${PREFIX}/share/${PACKAGE_TITLE}
+DOCPATH=${PREFIX}/share/doc/${PACKAGE_TITLE}
+
+INSTALL="install -D -m 644"
+INSTALL_X="install -D -m 755"
+
+rm -rf ${DESTDIR}${PREFIX}
+mkdir -p ${DESTDIR}${PREFIX}
+mkdir -p ${DESTDIR}${BINPATH}
+mkdir -p ${DESTDIR}${LIBPATH}
+mkdir -p ${DESTDIR}${SYSCONFDIR}
+
+PREPACKAGED="target/kafka-rest-${VERSION}-package"
+pushd ${PREPACKAGED}
+find . -type f | grep -v README[.]rpm | xargs -I XXX ${INSTALL} -o root -g root XXX ${DESTDIR}${PREFIX}/XXX
+popd
