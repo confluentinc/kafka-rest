445de6f5967d3d38e13139ddef9d6bb41febeec0
Ewen Cheslack-Postava <ewen@confluent.io>
2016-04-30 12:08:39 -0700

    Minor fixes for compatibility with newest 0.10.0 branch.

--- src/test/java/io/confluent/kafkarest/integration/ClusterTestHarness.java
+++ src/test/java/io/confluent/kafkarest/integration/ClusterTestHarness.java
@@ -143,8 +143,8 @@ public abstract class ClusterTestHarness {
       final Option<SecurityProtocol> noInterBrokerSecurityProtocol = scala.Option.apply(null);
       Properties props = TestUtils.createBrokerConfig(
           i, zkConnect, false, false, TestUtils.RandomPort(), noInterBrokerSecurityProtocol,
-          noFile, true, false, TestUtils.RandomPort(), false, TestUtils.RandomPort(), false,
-          TestUtils.RandomPort(), Option.<String>empty());
+          noFile, Option.<Properties>empty(), true, false, TestUtils.RandomPort(), false,
+          TestUtils.RandomPort(), false, TestUtils.RandomPort(), Option.<String>empty());
       props.setProperty("auto.create.topics.enable", "false");
       // We *must* override this to use the port we allocated (Kafka currently allocates one port
       // that it always uses for ZK
