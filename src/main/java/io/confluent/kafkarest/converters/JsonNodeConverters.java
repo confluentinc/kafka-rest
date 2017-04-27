fcfbbb3985d87a08e16dfd721ba13486febb9a3b
simplesteph <stephane.maarek@gmail.com>
2017-04-27 13:50:48 +1000

    optimisations and naming changes thanks to @kdrakon

--- src/main/java/io/confluent/kafkarest/converters/JsonNodeConverters.java
+++ src/main/java/io/confluent/kafkarest/converters/JsonNodeConverters.java
@@ -10,46 +10,43 @@ import java.util.Objects;
 
 public class JsonNodeConverters {
 
-    static JsonNode textToDecimal(JsonNode jsonNode, Schema schema) {
-        if (jsonNode instanceof TextNode){
-            String encodedBigDecimal = jsonNode.asText();
-            int scale = schema.getJsonProps().get("scale").asInt();
-            BigDecimal bd = BigDecimalDecoder.fromEncodedString(encodedBigDecimal, scale);
-            return new DecimalNode(bd);
-        } else {
-            return jsonNode;
+
+    static JsonNodeConverter withDecimalNodesAsText = new JsonNodeConverter() {
+        @Override
+        public JsonNode convert(JsonNode jsonNode, Schema schema) {
+            if (jsonNode instanceof DoubleNode) {
+                BigDecimal bd = jsonNode.decimalValue();
+                int scale = schema.getJsonProps().get("scale").asInt();
+                int precision = schema.getJsonProps().get("precision").asInt();
+                String bdBytesAsUtf8 = BigDecimalEncoder.toEncodedString(bd, scale, precision);
+                return new TextNode(bdBytesAsUtf8);
+            } else {
+                return jsonNode;
+            }
         }
     };
 
-    static JsonNode decimalToText(JsonNode jsonNode, Schema schema) {
-        if (jsonNode instanceof DoubleNode) {
-            BigDecimal bd = jsonNode.decimalValue();
-            int scale = schema.getJsonProps().get("scale").asInt();
-            int precision = schema.getJsonProps().get("precision").asInt();
-            String bdBytesAsUtf8 = BigDecimalEncoder.toEncodedString(bd, scale, precision);
-            return new TextNode(bdBytesAsUtf8);
-        } else {
-            return jsonNode;
-        }
-    }
-
-
-    private void dummy() {
-        transformJsonNode(null, null, new JsonNodeConverter() {
-            @Override
-            public JsonNode convert(JsonNode jsonNode, Schema schema) {
-                return textToDecimal(jsonNode, schema);
+    static JsonNodeConverter withTextNodesAsDecimal = new JsonNodeConverter() {
+        @Override
+        public JsonNode convert(JsonNode jsonNode, Schema schema) {
+            if (jsonNode instanceof TextNode) {
+                String encodedBigDecimal = jsonNode.asText();
+                int scale = schema.getJsonProps().get("scale").asInt();
+                BigDecimal bd = BigDecimalDecoder.fromEncodedString(encodedBigDecimal, scale);
+                return new DecimalNode(bd);
+            } else {
+                return jsonNode;
             }
-        });
-    }
+        }
+    };
 
     //  private static JsonNode transformJsonNode(JsonNode jsonNode, Schema schema) {
     static JsonNode transformJsonNode(JsonNode jsonNode, Schema schema, JsonNodeConverter jsonNodeConverter) {
         if (schema.getType() == Schema.Type.BYTES && schema.getJsonProps().containsKey("logicalType") &&
-                Objects.equals(schema.getJsonProps().get("logicalType").asText(), "decimal"))  {
+                Objects.equals(schema.getJsonProps().get("logicalType").asText(), "decimal")) {
             return jsonNodeConverter.convert(jsonNode, schema);
         } else if (schema.getType() == Schema.Type.RECORD) {
-            for (Schema.Field s : schema.getFields()){
+            for (Schema.Field s : schema.getFields()) {
                 JsonNode transformed = transformJsonNode(jsonNode.get(s.name()), s.schema(), jsonNodeConverter);
                 ((ObjectNode) jsonNode).set(s.name(), transformed);
             }
@@ -57,7 +54,7 @@ public class JsonNodeConverters {
             if (jsonNode.has("bytes") && jsonNode.get("bytes").isNumber()) {
                 for (Schema subSchema : schema.getTypes()) {
                     if (subSchema.getType() == Schema.Type.BYTES && subSchema.getJsonProps().containsKey("logicalType") &&
-                            Objects.equals(subSchema.getJsonProps().get("logicalType").asText(), "decimal")){
+                            Objects.equals(subSchema.getJsonProps().get("logicalType").asText(), "decimal")) {
                         JsonNode transformed = transformJsonNode(jsonNode.get("bytes"), subSchema, jsonNodeConverter);
                         ((ObjectNode) jsonNode).set("bytes", transformed);
                     }
