1795098c2c2a2da97e295d1581caf47188da7277
Ewen Cheslack-Postava <ewen@confluent.io>
2017-09-29 13:38:35 -0700

    Remove unused imports

--- src/main/java/io/confluent/kafkarest/KafkaRestMain.java
+++ src/main/java/io/confluent/kafkarest/KafkaRestMain.java
@@ -16,13 +16,11 @@
 
 package io.confluent.kafkarest;
 
-import org.eclipse.jetty.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-import io.confluent.kafkarest.extension.RestResourceExtension;
 import io.confluent.rest.RestConfigException;
 
 
