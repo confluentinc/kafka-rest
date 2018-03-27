b4a02f708a628f249fa6753030bbe60a4b33ccb6
Jon Court <jonathan.court@airnz.co.nz>
2018-03-27 16:38:11 +1300

    fixing tests for XHeader Reflection

---
+++ src/test/java/io/confluent/kafkarest/integration/XHeadersAreReflectedDisabledTest.java
@@ -0,0 +1,45 @@
+package io.confluent.kafkarest.integration;
+
+import io.confluent.kafkarest.Versions;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.confluent.kafkarest.TestUtils.assertOKResponse;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Verifies that the XHeaderReflectingResponseFilter is engaged when 'enable.reflect.xheaders'
+ * property is set to 'true'.
+ * <p>
+ * This is non-exhaustive - testing only a sample set of API calls.
+ */
+public class XHeadersAreReflectedDisabledTest extends ClusterTestHarness {
+
+  public XHeadersAreReflectedDisabledTest() {
+    super(1, false);
+  }
+
+  @Before
+  public void setup() throws Exception {
+    restProperties.put("enable.reflect.xheaders", String.valueOf(false));
+    super.setUp();
+  }
+
+  @Test
+  public void requestXHeaderIsNotReflectedInResponseWhenNotEnabled() {
+
+    // given x-header reflection is disabled and...
+    final String xHeaderKey = "x-some-arbitrary-header";
+    final String xHeaderValue = "some-value";
+
+    // when
+    Response response = request("/topics").header(xHeaderKey, xHeaderValue).get();
+    assertOKResponse(response, Versions.KAFKA_MOST_SPECIFIC_DEFAULT);
+
+    // then
+    assertFalse(response.getHeaders().containsKey(xHeaderKey));
+  }
+}
---
+++ src/test/java/io/confluent/kafkarest/integration/XHeadersAreReflectedEnabledTest.java
@@ -0,0 +1,61 @@
+package io.confluent.kafkarest.integration;
+
+import io.confluent.kafkarest.Versions;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.confluent.kafkarest.TestUtils.assertOKResponse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Verifies that the XHeaderReflectingResponseFilter is engaged when 'enable.reflect.xheaders'
+ * property is set to 'true'.
+ * <p>
+ * This is non-exhaustive - testing only a sample set of API calls.
+ */
+public class XHeadersAreReflectedEnabledTest extends ClusterTestHarness {
+
+  public XHeadersAreReflectedEnabledTest() {
+    super(1, false);
+  }
+
+  @Before
+  public void setup() throws Exception {
+    restProperties.put("enable.reflect.xheaders", String.valueOf(true));
+    super.setUp();
+  }
+
+  @Test
+  public void requestXHeaderIsReflectedInResponseWhenEnabled() {
+
+    // given x-header reflection is enabled and...
+    final String xHeaderKey = "x-some-arbitrary-header";
+    final String xHeaderValue = "some-value";
+
+    // when
+    Response response = request("/topics").header(xHeaderKey, xHeaderValue).get();
+    assertOKResponse(response, Versions.KAFKA_MOST_SPECIFIC_DEFAULT);
+
+    // then
+    assertEquals(xHeaderValue, response.getHeaders().getFirst(xHeaderKey));
+  }
+
+  @Test
+  public void requestNonXHeaderIsNotReflectedInResponseWhenEnabled() {
+
+    // given x-header reflection is enabled and...
+    final String nonXHeaderKey = "some-arbitrary-header";
+    final String nonXHeaderValue = "some-value";
+
+    // when
+    Response response = request("/topics").header(nonXHeaderKey, nonXHeaderValue).get();
+    assertOKResponse(response, Versions.KAFKA_MOST_SPECIFIC_DEFAULT);
+
+    // then
+    assertFalse(response.getHeaders().containsKey(nonXHeaderKey));
+  }
+}
