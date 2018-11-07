6f59d3a976bf82eb1e2709adffca0ff7d92a0220
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2018-11-07 15:52:12 +0000

    CPKAFKA-1871: Fix problem where ReadTaskSchedulerThread would loop too often

--- kafka-rest/src/main/java/io/confluent/kafkarest/v2/KafkaConsumerManager.java
+++ kafka-rest/src/main/java/io/confluent/kafkarest/v2/KafkaConsumerManager.java
@@ -386,7 +386,7 @@ public class KafkaConsumerManager {
     @Override
     public long getDelay(TimeUnit unit) {
       long delayMs = waitExpirationMs - config.getTime().milliseconds();
-      return TimeUnit.MILLISECONDS.convert(delayMs, unit);
+      return unit.convert(delayMs, TimeUnit.MILLISECONDS);
     }
 
     @Override
