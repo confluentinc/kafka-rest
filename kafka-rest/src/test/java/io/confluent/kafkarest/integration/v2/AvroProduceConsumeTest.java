42ba1abb807c1e1cf022dc9534e7ec2776925d9f
Robert Yokota <rayokota@gmail.com>
2020-02-10 19:41:12 -0800

    Add produce-consumer tests for Protobuf/JSON Schema

--- kafka-rest/src/test/java/io/confluent/kafkarest/integration/v2/AvroProduceConsumeTest.java
+++ kafka-rest/src/test/java/io/confluent/kafkarest/integration/v2/AvroProduceConsumeTest.java
@@ -1,43 +1,20 @@
 package io.confluent.kafkarest.integration.v2;
 
-import static java.util.Collections.singletonList;
-import static java.util.Collections.unmodifiableMap;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.IntNode;
+import io.confluent.kafka.schemaregistry.ParsedSchema;
+import io.confluent.kafka.schemaregistry.avro.AvroSchema;
 import io.confluent.kafkarest.Versions;
 import io.confluent.kafkarest.converters.AvroConverter;
-import io.confluent.kafkarest.entities.ConsumerInstanceConfig;
-import io.confluent.kafkarest.entities.ConsumerSubscriptionRecord;
-import io.confluent.kafkarest.entities.CreateConsumerInstanceResponse;
 import io.confluent.kafkarest.entities.EmbeddedFormat;
-import io.confluent.kafkarest.entities.SchemaConsumerRecord;
 import io.confluent.kafkarest.entities.SchemaTopicProduceRecord;
-import io.confluent.kafkarest.entities.TopicProduceRequest;
-import io.confluent.kafkarest.integration.ClusterTestHarness;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecordBuilder;
-import org.junit.Test;
 
-public final class AvroProduceConsumeTest extends ClusterTestHarness {
+public final class AvroProduceConsumeTest extends SchemaProduceConsumeTest {
 
   private static final AvroConverter AVRO_CONVERTER = new AvroConverter();
-
-  private static final String TOPIC = "topic-1";
-
-  private static final String CONSUMER_GROUP = "group-1";
 
   private static final Schema KEY_SCHEMA =
       new Schema.Parser().parse(""
@@ -79,115 +56,28 @@ public final class AvroProduceConsumeTest extends ClusterTestHarness {
     super(/* numBrokers= */ 1, /* withSchemaRegistry= */ true);
   }
 
-  @Test
-  public void produceThenConsume_returnsExactlyProduced() {
-    createTopic(TOPIC, /* numPartitions= */ 1, /* replicationFactor= */ (short) 1);
-
-    Response createConsumerInstanceResponse =
-        request(String.format("/consumers/%s", CONSUMER_GROUP))
-            .post(
-                Entity.entity(
-                    new ConsumerInstanceConfig(EmbeddedFormat.AVRO), Versions.KAFKA_V2_JSON));
-
-    assertEquals(Status.OK.getStatusCode(), createConsumerInstanceResponse.getStatus());
-
-    CreateConsumerInstanceResponse createConsumerInstance =
-        createConsumerInstanceResponse.readEntity(CreateConsumerInstanceResponse.class);
-
-    Response subscribeResponse =
-        request(
-            String.format(
-                "/consumers/%s/instances/%s/subscription",
-                CONSUMER_GROUP,
-                createConsumerInstance.getInstanceId()))
-            .post(
-                Entity.entity(
-                    new ConsumerSubscriptionRecord(
-                        singletonList(TOPIC), null), Versions.KAFKA_V2_JSON));
-
-    assertEquals(Status.NO_CONTENT.getStatusCode(), subscribeResponse.getStatus());
-
-    // Needs to consume empty once before producing.
-    request(
-        String.format(
-            "/consumers/%s/instances/%s/records",
-            CONSUMER_GROUP,
-            createConsumerInstance.getInstanceId()))
-        .accept(Versions.KAFKA_V2_JSON_AVRO)
-        .get();
-
-    TopicProduceRequest<SchemaTopicProduceRecord> produceRequest = new TopicProduceRequest<>();
-    produceRequest.setRecords(PRODUCE_RECORDS);
-    produceRequest.setKeySchema(KEY_SCHEMA.toString());
-    produceRequest.setValueSchema(VALUE_SCHEMA.toString());
-
-    Response produceResponse =
-        request(String.format("/topics/%s", TOPIC))
-            .post(Entity.entity(produceRequest, Versions.KAFKA_V2_JSON_AVRO));
-
-    assertEquals(Status.OK.getStatusCode(), produceResponse.getStatus());
-
-    Response readRecordsResponse =
-        request(
-            String.format(
-                "/consumers/%s/instances/%s/records",
-                CONSUMER_GROUP,
-                createConsumerInstance.getInstanceId()))
-            .accept(Versions.KAFKA_V2_JSON_AVRO)
-            .get();
-
-    assertEquals(Status.OK.getStatusCode(), readRecordsResponse.getStatus());
-
-    List<SchemaConsumerRecord> readRecords =
-        readRecordsResponse.readEntity(new GenericType<List<SchemaConsumerRecord>>() {
-        });
-
-    assertMapEquals(producedToMap(PRODUCE_RECORDS), consumedToMap(readRecords));
+  @Override
+  protected EmbeddedFormat getFormat() {
+    return EmbeddedFormat.AVRO;
   }
 
-  private static final Map<JsonNode, JsonNode> producedToMap(
-      List<SchemaTopicProduceRecord> records) {
-    HashMap<JsonNode, JsonNode> map = new HashMap<>();
-    for (SchemaTopicProduceRecord record : records) {
-      map.put(record.getKey(), record.getValue());
-    }
-    return unmodifiableMap(map);
+  @Override
+  protected String getContentType() {
+    return Versions.KAFKA_V2_JSON_AVRO;
   }
 
-  private static Map<JsonNode, JsonNode> consumedToMap(List<SchemaConsumerRecord> records) {
-    HashMap<JsonNode, JsonNode> map = new HashMap<>();
-    for (SchemaConsumerRecord record : records) {
-      map.put(record.getKey(), record.getValue());
-    }
-    return unmodifiableMap(map);
+  @Override
+  protected ParsedSchema getKeySchema() {
+    return new AvroSchema(KEY_SCHEMA);
   }
 
-  private static <K, V> void assertMapEquals(Map<K, V> expected, Map<K, V> actual) {
-    HashSet<K> extra = new HashSet<>();
-    HashSet<K> missing = new HashSet<>();
-    HashSet<K> different = new HashSet<>();
+  @Override
+  protected ParsedSchema getValueSchema() {
+    return new AvroSchema(VALUE_SCHEMA);
+  }
 
-    for (Entry<K, V> entry : expected.entrySet()) {
-      if (!actual.containsKey(entry.getKey())) {
-        missing.add(entry.getKey());
-      } else if (!actual.get(entry.getKey()).equals(entry.getValue())) {
-        different.add(entry.getKey());
-      }
-    }
-
-    for (K key : actual.keySet()) {
-      if (!expected.containsKey(key)) {
-        extra.add(key);
-      }
-    }
-
-    if (!extra.isEmpty() || !missing.isEmpty() || !different.isEmpty()) {
-      fail(
-          String.format(
-              "Expected and actual are not equal. Extra: %s, Missing: %s, Different: %s",
-              extra,
-              missing,
-              different));
-    }
+  @Override
+  protected List<SchemaTopicProduceRecord> getProduceRecords() {
+    return PRODUCE_RECORDS;
   }
 }
---
+++ kafka-rest/src/test/java/io/confluent/kafkarest/integration/v2/JsonSchemaProduceConsumeTest.java
@@ -0,0 +1,77 @@
+package io.confluent.kafkarest.integration.v2;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import io.confluent.kafka.schemaregistry.ParsedSchema;
+import io.confluent.kafka.schemaregistry.json.JsonSchema;
+import io.confluent.kafkarest.Versions;
+import io.confluent.kafkarest.converters.JsonSchemaConverter;
+import io.confluent.kafkarest.entities.EmbeddedFormat;
+import io.confluent.kafkarest.entities.SchemaTopicProduceRecord;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class JsonSchemaProduceConsumeTest extends SchemaProduceConsumeTest {
+
+  private static final JsonSchemaConverter JSON_SCHEMA_CONVERTER = new JsonSchemaConverter();
+
+  private static final JsonSchema KEY_SCHEMA =
+      new JsonSchema("{\"type\":\"number\"}");
+
+  private static final JsonSchema VALUE_SCHEMA =
+      new JsonSchema("{\"type\":\"object\",\"properties\":{\"value\":{\"type\":\"number\"}}}");
+
+  private static final List<SchemaTopicProduceRecord> PRODUCE_RECORDS =
+      Arrays.asList(
+          new SchemaTopicProduceRecord(
+              new IntNode(1),
+              JSON_SCHEMA_CONVERTER.toJson(getMessage(11)).getJson(),
+              /* partition= */ 0),
+          new SchemaTopicProduceRecord(
+              new IntNode(2),
+              JSON_SCHEMA_CONVERTER.toJson(getMessage(12)).getJson(),
+              /* partition= */ 0),
+          new SchemaTopicProduceRecord(
+              new IntNode(3),
+              JSON_SCHEMA_CONVERTER.toJson(getMessage(13)).getJson(),
+              /* partition= */ 0));
+
+  private static JsonNode getMessage(int value) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    ObjectNode root = objectMapper.createObjectNode();
+    root.put("value", value);
+    return root;
+  }
+
+  public JsonSchemaProduceConsumeTest() {
+    super(/* numBrokers= */ 1, /* withSchemaRegistry= */ true);
+  }
+
+  @Override
+  protected EmbeddedFormat getFormat() {
+    return EmbeddedFormat.JSONSCHEMA;
+  }
+
+  @Override
+  protected String getContentType() {
+    return Versions.KAFKA_V2_JSON_JSON_SCHEMA;
+  }
+
+  @Override
+  protected ParsedSchema getKeySchema() {
+    return KEY_SCHEMA;
+  }
+
+  @Override
+  protected ParsedSchema getValueSchema() {
+    return VALUE_SCHEMA;
+  }
+
+  @Override
+  protected List<SchemaTopicProduceRecord> getProduceRecords() {
+    return PRODUCE_RECORDS;
+  }
+}
