55c4135bb2c629d716489d66de8a37c2503aaee0
Aishwarya <aishwarya.gune@Aishwarya-Gunes-MBP13.local>
2020-03-02 14:53:39 -0800

    using topicinfo different entity

--- kafka-rest/src/main/java/io/confluent/kafkarest/controllers/TopicManager.java
+++ kafka-rest/src/main/java/io/confluent/kafkarest/controllers/TopicManager.java
@@ -1,14 +1,10 @@
 package io.confluent.kafkarest.controllers;
 
 import io.confluent.kafkarest.entities.Topic;
-import io.confluent.kafkarest.entities.TopicsMap;
-import org.apache.kafka.clients.admin.TopicListing;
+import io.confluent.kafkarest.entities.TopicInfo;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 /**
  * A service to manage Kafka {@link Topic Topics}.
@@ -17,7 +13,7 @@ public interface TopicManager {
     /**
     Returns the list of Kafka {@link Topic Topics} known.
      */
-    CompletableFuture<TopicsMap> listTopics(String clusterId) throws ExecutionException, InterruptedException;
+    CompletableFuture<List<Topic>> listTopics(String clusterId);
 
     CompletableFuture<List<Topic>> getTopic(String clusterId, String topicName);
 }
---
+++ kafka-rest/src/main/java/io/confluent/kafkarest/entities/v3/ListTopicsResponse.java
@@ -0,0 +1,52 @@
+package io.confluent.kafkarest.entities.v3;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+/**
+ * Response body for {@code GET /topics} requests
+ */
+public class ListTopicsResponse {
+    private final CollectionLink links;
+    private final List<TopicData> data;
+
+    public ListTopicsResponse(CollectionLink links, List<TopicData> data) {
+        this.links = Objects.requireNonNull(links);
+        this.data = Objects.requireNonNull(data);
+    }
+
+    @JsonProperty("links")
+    public CollectionLink getLinks() {
+        return links;
+    }
+
+    @JsonProperty("data")
+    public List<TopicData> getData() {
+        return data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListTopicsResponse that = (ListTopicsResponse) o;
+        return Objects.equals(links, that.links) &&
+                Objects.equals(data, that.data);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(links, data);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", ListTopicsResponse.class.getSimpleName() + "[", "]")
+                .add("links=" + links)
+                .add("data=" + data)
+                .toString();
+    }
+}
