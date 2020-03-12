775870f8a8b10b4bda1a5e28575fe1d3a34be085
Rigel Bezerra de Melo <rigelbm@gmail.com>
2020-03-12 23:44:53 +0000

    Fix checkstyle violation.

--- kafka-rest/src/main/java/io/confluent/kafkarest/controllers/PartitionManagerImpl.java
+++ kafka-rest/src/main/java/io/confluent/kafkarest/controllers/PartitionManagerImpl.java
@@ -24,9 +24,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 import javax.inject.Inject;
-import javax.ws.rs.NotFoundException;
 
 class PartitionManagerImpl implements PartitionManager {
 
