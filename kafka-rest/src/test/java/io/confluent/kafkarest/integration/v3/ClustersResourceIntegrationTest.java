a9ebbf74ae98a3d74e172dd6954ba601d81674d6
Aishwarya <aishwarya.gune@Aishwarya-Gunes-MBP13.local>
2020-06-16 13:19:57 +0530

    nit modify test

--- kafka-rest/src/test/java/io/confluent/kafkarest/integration/v3/ClustersResourceIntegrationTest.java
+++ kafka-rest/src/test/java/io/confluent/kafkarest/integration/v3/ClustersResourceIntegrationTest.java
@@ -119,6 +119,10 @@ public class ClustersResourceIntegrationTest extends ClusterTestHarness {
                 .setTopicConfigs(
                     Resource.Relationship.create(
                         baseUrl + "/v3/clusters/" + clusterId + "/topic-configs"))
+                .setReassignment(
+                    Resource.Relationship.create(
+                        baseUrl + "/v3/clusters/" + clusterId +
+                            "/topics/-/partitions/-/reassignment"))
                 .build());
 
     Response response =
