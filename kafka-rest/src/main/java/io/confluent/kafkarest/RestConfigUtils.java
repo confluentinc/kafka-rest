859d4125f1b779a53b20685404cb1a549d10ec55
Ismael Juma <ismael@juma.me.uk>
2020-08-17 22:18:48 -0700

    Remove dependency on core kafka jar
    We should only depend on the kafka-clients jar.

--- kafka-rest/src/main/java/io/confluent/kafkarest/RestConfigUtils.java
+++ kafka-rest/src/main/java/io/confluent/kafkarest/RestConfigUtils.java
@@ -16,64 +16,11 @@
 package io.confluent.kafkarest;
 
 import static io.confluent.kafkarest.KafkaRestConfig.BOOTSTRAP_SERVERS_CONFIG;
-import static io.confluent.kafkarest.KafkaRestConfig.KAFKACLIENT_ZK_SESSION_TIMEOUT_MS_CONFIG;
-import static io.confluent.kafkarest.KafkaRestConfig.ZOOKEEPER_CONNECT_CONFIG;
-
-import java.util.List;
-
-import kafka.cluster.Broker;
-import kafka.cluster.EndPoint;
-import kafka.zk.KafkaZkClient;
-import kafka.zookeeper.ZooKeeperClient;
-import org.apache.kafka.common.security.JaasUtils;
-import org.apache.kafka.common.utils.Time;
-import org.apache.kafka.common.utils.Utils;
-import org.eclipse.jetty.util.StringUtil;
-import scala.collection.JavaConverters;
-import scala.collection.Seq;
 
 public class RestConfigUtils {
 
   public static String bootstrapBrokers(KafkaRestConfig config) {
-    int zkSessionTimeoutMs = config.getInt(KAFKACLIENT_ZK_SESSION_TIMEOUT_MS_CONFIG);
-
-    String bootstrapServersConfig = config.getString(BOOTSTRAP_SERVERS_CONFIG);
-    if (StringUtil.isNotBlank(bootstrapServersConfig)) {
-      return bootstrapServersConfig;
-    }
-    KafkaZkClient zkClient = null;
-    try {
-      org.apache.kafka.common.utils.Time time = Time.SYSTEM;
-
-      zkClient = new KafkaZkClient(
-          new ZooKeeperClient(config.getString(ZOOKEEPER_CONNECT_CONFIG), zkSessionTimeoutMs,
-              zkSessionTimeoutMs, Integer.MAX_VALUE, time,
-              "testMetricGroup", "testMetricGroupType"),
-          JaasUtils.isZkSaslEnabled(),
-          time);
-      return getBootstrapBrokers(zkClient);
-    } finally {
-      if (zkClient != null) {
-        zkClient.close();
-      }
-    }
+    return config.getString(BOOTSTRAP_SERVERS_CONFIG);
   }
 
-  private static String getBootstrapBrokers(KafkaZkClient zkClient) {
-    Seq<Broker> brokerSeq = zkClient.getAllBrokersInCluster();
-
-    List<Broker> brokers = JavaConverters.seqAsJavaList(brokerSeq);
-    String bootstrapBrokers = "";
-    for (int i = 0; i < brokers.size(); i++) {
-      for (EndPoint ep : JavaConverters.asJavaCollection(brokers.get(i).endPoints())) {
-        if (bootstrapBrokers.length() > 0) {
-          bootstrapBrokers += ",";
-        }
-        String hostport =
-            ep.host() == null ? ":" + ep.port() : Utils.formatAddress(ep.host(), ep.port());
-        bootstrapBrokers += ep.securityProtocol() + "://" + hostport;
-      }
-    }
-    return bootstrapBrokers;
-  }
 }
