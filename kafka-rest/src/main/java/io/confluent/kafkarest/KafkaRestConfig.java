9c6e9e8cf7e10d7e68996b3fc3609f1618bf0271
Rigel Bezerra de Melo <rigelbm@gmail.com>
2022-02-16 12:05:34 +0000

    Disable producer idempotency until KAFKA-13668 is fixed.
    For the idempotent producer, some errors are permanent, meaning the producer instance becomes unusable afterwards. Authorization errors are one of these. This behaviour disagrees with the Kafka REST model, which assumes the producer to be stateless across requests. Until we either adapt Kafka REST to the idempotent model, or authorization errors become non-fatal, we will disable idempotent producers in Kafka REST.

--- kafka-rest/src/main/java/io/confluent/kafkarest/KafkaRestConfig.java
+++ kafka-rest/src/main/java/io/confluent/kafkarest/KafkaRestConfig.java
@@ -873,6 +873,14 @@ public class KafkaRestConfig extends RestConfig {
 
     Properties producerProperties = new Properties();
     producerProperties.putAll(producerConfigs);
+
+    // KREST-4606: Disable idempotency until KAFKA-13668 is fixed.
+    if (Boolean.parseBoolean(producerProperties.getProperty("enable.idempotence"))) {
+      log.warn(
+          "Idempotent producers are not supported in Kafka REST. Ignoring enable.idempotence'.");
+    }
+    producerProperties.put("enable.idempotence", "false");
+
     addTelemetryReporterProperties(producerProperties);
 
     return producerProperties;
