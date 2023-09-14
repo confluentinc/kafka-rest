ff4401a9b98fcca88310d3233bfb456e4a663789
Truc Nguyen <trnguyen@confluent.io>
2023-09-14 10:59:20 +0100

    KREST-10285 example rest produce v3 streaming in javascript (nodejs)

---
+++ examples/produce_v3/javascript/main.js
@@ -0,0 +1,52 @@
+// if the endpoint is https, please use https package
+const http = require('http');
+
+// handling streaming response
+var recordReceiptCounter = 1;
+callback = function(response) {
+    response.on(
+        'data',
+        function (data) {
+            let chunk = data.toString('utf8');
+            if (chunk.trim()) {
+                console.log("Receipt for record #" + recordReceiptCounter + " is");
+                console.log(JSON.parse(chunk));
+                recordReceiptCounter += 1;
+            }
+        });
+
+    response.on('end', function () {
+        console.log("Stream is closed!");
+    });
+}
+
+async function main() {
+    // please update the below information to fit your Kafka Rest endpoint
+    const host = 'localhost';
+    const port = '8082';
+    const clusterId = 'ZXBWzl8VQ5WTB_hgEAuGeQ';
+    const topic = 'topic_1';
+    let options = {
+        host: host,
+        port: port,
+        path: `/v3/clusters/${clusterId}/topics/${topic}/records`,
+        method: 'POST',
+        headers: {'Transfer-Encoding': 'chunked', 'Content-Type': 'application/json'}
+    };
+    let request = http.request(options, callback);
+    // produces records to a Kafka topic, using Kafka REST's produce API -
+    // /v3/clusters/{clusterId}/topics/{topic}/records.
+    for (let i = 1; i <= 10; i++) {
+      var record = JSON.stringify({"value": {"type": "JSON", "data": {"foo": i } }});
+      console.log("Producing record #" + i + " with json " + record);
+      request.write(record);
+
+      // Wait 1 sec to confirm we indeed are streaming data back
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+
+    // Close the stream.
+    request.end();
+}
+
+main();
