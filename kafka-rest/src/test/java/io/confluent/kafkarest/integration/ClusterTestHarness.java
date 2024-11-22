af4cd556413f663fb5cddc74941263386ac0b409
Truc Nguyen <trnguyen@confluent.io>
2024-11-22 12:54:58 +0000

    Fix visibility for stopRest function

--- kafka-rest/src/test/java/io/confluent/kafkarest/integration/ClusterTestHarness.java
+++ kafka-rest/src/test/java/io/confluent/kafkarest/integration/ClusterTestHarness.java
@@ -287,7 +287,7 @@ public abstract class ClusterTestHarness {
     restServer.start();
   }
 
-  private void stopRest() throws Exception {
+  protected void stopRest() throws Exception {
     log.info("Stopping REST.");
     if (restApp != null) {
       restApp.stop();
