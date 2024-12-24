39a2e26881d7113c81752a7f49f4c718b521c1e0
dongnuolyu <dlyu@confluent.io>
2024-12-23 23:50:22 -0500

    throw exception to log description

--- kafka-rest/src/main/java/io/confluent/kafkarest/controllers/ConsumerGroupManagerImpl.java
+++ kafka-rest/src/main/java/io/confluent/kafkarest/controllers/ConsumerGroupManagerImpl.java
@@ -76,7 +76,9 @@ final class ConsumerGroupManagerImpl implements ConsumerGroupManager {
         .thenApply(
             descriptions -> {
               if (descriptions != null) {
-                throw new IllegalStateException("Description: " + descriptions);
+                throw new IllegalStateException("Description values: " + descriptions.values() + ", "
+                 + "Description state: " + descriptions.values().stream().map(ConsumerGroup::state) + ", "
+                 + "Description assignor: " + descriptions.values().stream().map(ConsumerGroup::partitionAssignor));
               }
               return descriptions.values().stream()
                   .filter(
