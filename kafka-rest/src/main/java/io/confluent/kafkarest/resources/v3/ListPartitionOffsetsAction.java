de3f8b5248c0d20f0528007a301795203297ca70
Apurva Purushotama <apurushotama@confluent.io>
2025-01-29 18:51:57 -0600

    Add tests

--- kafka-rest/src/main/java/io/confluent/kafkarest/resources/v3/ListPartitionOffsetsAction.java
+++ kafka-rest/src/main/java/io/confluent/kafkarest/resources/v3/ListPartitionOffsetsAction.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package io.confluent.kafkarest.resources.v3;
 
 import static java.util.Objects.requireNonNull;
@@ -5,7 +20,9 @@ import static java.util.Objects.requireNonNull;
 import io.confluent.kafkarest.Errors;
 import io.confluent.kafkarest.controllers.PartitionManager;
 import io.confluent.kafkarest.entities.Partition;
-import io.confluent.kafkarest.entities.v3.*;
+import io.confluent.kafkarest.entities.v3.ListPartitionOffsetsResponse;
+import io.confluent.kafkarest.entities.v3.PartitionWithOffsetsData;
+import io.confluent.kafkarest.entities.v3.Resource;
 import io.confluent.kafkarest.extension.ResourceAccesslistFeature.ResourceName;
 import io.confluent.kafkarest.resources.AsyncResponses;
 import io.confluent.kafkarest.response.CrnFactory;
@@ -14,7 +31,9 @@ import io.confluent.rest.annotations.PerformanceMetric;
 import java.util.concurrent.CompletableFuture;
 import javax.inject.Inject;
 import javax.inject.Provider;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
 
